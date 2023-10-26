a9fe774adf552e07f8267c906d5d1b03902b0fb5
AliyevaTukazban <tukazbanaliyeva@yahoo.com>
2023-10-26 18:05:49 +0200

    26.10.23

--- src/main/java/de/htwberlin/webtech/helloworldcontroller.java
+++ src/main/java/de/htwberlin/webtech/helloworldcontroller.java
@@ -1,14 +1,14 @@
-package de.htwberlin.webtech.web;
+package de.htwberlin.webtech;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class helloworldcontroller {
-    @GetMapping(path = "/")
-    public ModelAndView showHelloWorldPage(){
-        return new ModelAndView("helloworld");
-
+    @GetMapping("/")
+    public String index() {
+        return "Hallo World!";
     }
 }
